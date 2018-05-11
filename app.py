80d1fe68e65a731beee0a4e8cb5b094b1322e510
Liam Barnett <liam.barnett@uk.fujitsu.com>
2018-05-11 16:32:26 +0100

    Addedd comments, support the dynamodb events, sigmoid activation function

--- app.py
+++ app.py
@@ -1,34 +1,65 @@
-import sys
-import boto3
+import os
+import json
 import uuid
+import boto3
+import math
+
+# Get the service resource
+#dynamodb = boto3.resource('dynamodb')
+
+#table = dynamodb.Table(os.environ['TABLE_NAME'])
+#output_table = dynamodb.Table(os.environ['OUTPUT_TABLE_NAME'])
 
 # Set array values - TODO Get Arrays from external data source
-value = [1, 2, 0, 4, 2]
-weight = [5, 6, 7, 8, 8]
+# value = [1, 2, 0, 4, 2]
+# weight = [5, 6, 7, 8, 8]
+
+'''
+Inserts data into dynamodb
+'''
 
 
-def put_dynamo():
-    print('hi')
-
-    # Get the service resource
-    dynamodb = boto3.resource('dynamodb')
-
-    table = dynamodb.Table('helloai')
-
-    table.put_item(
+def put_dynamo(id, output):
+    output_table.put_item(
         Item={
-            'id': uuid.uuid4(),
-            'username': 'janedoe',
-            'first_name': 'Jane',
-            'last_name': 'Doe',
-            'age': 25,
-            'account_type': 'standard_user',
+            'id': id,
+            'value': output
         }
     )
 
-# Function which checks the length of each array
-def length_check():
+def put_test_data():
+  table.put_item(
+      Item={
+          'id': str(uuid.uuid4()),
+          'value': [1, 2, 0, 4, 2],
+          'weight': [5, 6, 7, 8, 8]
+      }
+  )
 
+'''
+Retrieves data from dynamodb
+'''
+
+
+def get_dynamo():
+    response = table.get_item(
+        Key={
+            'id': 'a11ce4e4-bbee-4119-a39c-025451eda0cd'
+        }
+    )
+    item = response['Item']
+    print(item)
+
+    return item
+
+
+'''
+Checks the length of the input arrays
+Returns a boolean value
+'''
+
+
+def length_check(weight, value):
     # Get length of each array
     value_length = len(value)
     weight_length = len(weight)
@@ -36,13 +67,20 @@ def length_check():
     # Check if arrays are the same length
     return False if value_length != weight_length else True
 
-
-def calculate():
+'''
+Sigmoid function required by the calculate function
+'''
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+'''
+The main calculations 
+'''
+def calculate(weight, value):
     # Initialise cumulative variable
     cumulative = 0
 
     # Check length of arrays are equal
-    if not length_check():
+    if not length_check(weight, value):
         print("ERROR lengths do not match")
         # TODO error handling
 
@@ -62,16 +100,99 @@ def calculate():
             print("WARNING 0 found - SKIPPING ITERATION")
             continue
 
-        # Multiple the value by the weight and add to the cumulative variable
+        # Multiply the value by the weight and add to the cumulative variable
         cumulative = cumulative + (x * y)
 
-        # Increment the index for the second array
+        # Increment the index for the weight array
         i = i + 1
 
-    # Print total value
+
+    # TODO CONVERT ME TO DECIMAL - DOES NOT CURRENTLY WORK
     print(cumulative)
-    return cumulative
-# TODO apply activation function to cumulative
+    output = sigmoid(cumulative)
+
+
+    print(output)
+
+    return output
+
+
+# TODO apply activation function to cumulative value
+
+'''
+Builds arrays from dynamodb event object
+'''
+def build_array(data):
+
+    list = []
+
+    for x in data:
+        list.append(int(x['N']))
+
+    return list
+
+'''
+Entry point for lambda
+'''
+def main(event, context):
+  #  put_test_data()
+  #  data = get_dynamo()
+
+    weight_list = []
+    value_list = []
+
+    # Select the single record from the event data
+    record = event['Records'][0]
+
+    # Filter to the required data
+    data = record['dynamodb']['NewImage']
+
+    # If the event type is insert then process, otherwise skip
+    if record['eventName'] == 'INSERT':
+        print('inside insert code')
+
+        # Pull required attributes from event object
+        id          = data['id']['S']
+        weight_data = data['weight']['L']
+        value_data  = data['value']['L']
+
+        # Convert the object array into a usuable python list
+        weight = build_array(weight_data)
+        value = build_array(value_data)
+
+        print(weight)
+        print(value)
+
+        # Run core calculations
+        output = calculate(weight, value)
+
+        # Write result to output table
+        put_dynamo(id, output)
+
+    else:
+        print('Not an insert event - Skipping')
+
+    body = {
+        "message": "Serverless function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "CumulativeValue": 'debug',
+    #    "Data": data
+    }
+
+    return response
+
+
+def main_local():
+    value = [1.1, 2, 0, 4.5, 2]
+    weight = [5, 6.7, 7, 8, -0.8]
+    print(weight)
+    output = calculate(weight, value)
+
 
 if __name__ == "__main__":
-    calculate()
+    main_local()
--- handler.py
+++ handler.py
@@ -2,40 +2,49 @@ import os
 import json
 import uuid
 import boto3
+import math
 
 # Get the service resource
 dynamodb = boto3.resource('dynamodb')
 
+# Retrieve table names from AWS environment variables
 table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-# Set array values - TODO Get Arrays from external data source
-value = [1, 2, 0, 4, 2]
-weight = [5, 6, 7, 8, 8]
+output_table = dynamodb.Table(os.environ['OUTPUT_TABLE_NAME'])
 
 '''
-Inserts data into dynamodb
-'''
-def put_dynamo():
+## NOT USED  ##
 
-   table.put_item(
-       Item={
-           'id': str(uuid.uuid4()),
-           'username': 'janedoe',
-           'first_name': 'Jane',
-           'last_name': 'Doe',
-           'age': 25,
-           'account_type': 'standard_user',
-       }
-   )
+Insert test data into dynamodb
+
+Parameters:
+    N/A
+Example output:
+    N/A
+'''
+def put_test_data():
+  table.put_item(
+      Item={
+          'id': str(uuid.uuid4()),
+          'value': [1, 2, 0, 4, 2],
+          'weight': [5, 6, 7, 8, 8]
+      }
+  )
 
 '''
-Retrieves data from dynamodb
+## NOT USED  ##
+
+Retrieves data from DynamoDB
+
+Parameters:
+    weight:
+    value:
+
+Example output:
 '''
 def get_dynamo():
-
     response = table.get_item(
         Key={
-            'id': '8854b233-5807-4cb0-afce-4636d3d3783f'
+            'id': 'a11ce4e4-bbee-4119-a39c-025451eda0cd'
         }
     )
     item = response['Item']
@@ -43,12 +52,38 @@ def get_dynamo():
 
     return item
 
+'''
+Inserts data into the output DynamoDB table
+
+Parameters:
+    id - the ID extracted from the DynamoDB record - creates a relationship between the input/ouput tables
+    output - the result from the calculation process
+
+Example output:
+    N/A
+'''
+def put_dynamo(id, output):
+    output_table.put_item(
+        Item={
+            'id': id,
+            'value': output
+        }
+    )
 
 '''
-Checks the length of the input arrays
-Returns a boolean value
+This function checks whether the two arrays used in the calculation process are the same length
+If they are equal in length True is returned, otherwise False
+
+Parameters:
+    weight: list containing the weight values
+    value: list containing the values
+
+Example output:
+    True/False
 '''
-def length_check():
+
+
+def length_check(weight, value):
     # Get length of each array
     value_length = len(value)
     weight_length = len(weight)
@@ -57,18 +92,59 @@ def length_check():
     return False if value_length != weight_length else True
 
 '''
-The main calculations 
+Runs the activation function against the output provided by the calculation function
+
+Parameters:
+    x: the cumulative value provided by the calculation function
+
+Example output:
+    float
 '''
-def calculate():
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+'''
+Builds a list of floats from a list of objects
+When retrieving a int list/array from DynamoDB, it is returned in the following format - [{'N': '5.5'}, {'N': '6'}, {'N': '7.6'}, {'N': '8'}, {'N': '8'}]
+This function converts this to a list of floats ready for the calculation function
+
+Parameters:
+    data - array of objects passed by DynamoDB
+    
+Example output:
+    [5.5, 6.0, 7.6, 8.0, 8.0]
+'''
+def build_array(data):
+
+    list = []
+
+    for x in data:
+        list.append(float(x['N']))
+
+    return list
+
+'''
+This is the main calculation function which multiplies the value from the weight array, with the value from the value array
+Iterations containing 0s are ignored and the function will error if the arrays are of varying lengths
+The result is returned to the main function
+
+Parameters:
+    weight: weight array of objects from DynamoDB
+    value:  value array of objects from DynamoDB
+
+Example output:
+    int - 1 or 0
+'''
+def calculate(weight, value):
     # Initialise cumulative variable
     cumulative = 0
 
     # Check length of arrays are equal
-    if not length_check():
+    if not length_check(weight, value):
         print("ERROR lengths do not match")
         # TODO error handling
 
-    # Set index variable
+    # Initialise index variable
     i = 0
 
     # Loop through the first array
@@ -76,8 +152,8 @@ def calculate():
         y = weight[i]
 
         # Debug output
-        print("Value = " + str(x))
-        print("Weight = " + str(y))
+        #print("Value = " + str(x))
+        #print("Weight = " + str(y))
 
         # Check if either value is equal to 0, if yes, skip to the next iteration
         if 0 in (x, y):
@@ -90,20 +166,74 @@ def calculate():
         # Increment the index for the weight array
         i = i + 1
 
-    print(cumulative)
-
-    return cumulative
+    output = sigmoid(cumulative)
 
 
-# TODO apply activation function to cumulative value
+    print(output)
+
+    return output
 
 '''
-Entry point for lambda
+## Entry point for the AWS Lambda function ##
+
+This function dissects the DynamoDB event and pulls out the required attributes ready for processing. 
+If the event is not of type 'INSERT', the process is skipped.
+ 
+Returns JSON back to the Lambda service
+
+Parameters:
+    event   - standard AWS Lambda parameter
+    context - standard AWS Lambda parameter
+    
+Example output:
+    {
+      "statusCode": 200,
+      "body": "{"message": "Serverless function executed successfully!},
+      "output": 2
+    }
 '''
 def main(event, context):
-    total = calculate()
-    put_dynamo()
-    user = get_dynamo()
+
+    print(event)
+
+    weight_list = []
+    value_list = []
+
+    # Select the single record from the event data
+    record = event['Records'][0]
+
+    # If the event type is insert then process, otherwise skip
+    if record['eventName'] == 'INSERT':
+        print('inside insert code')
+
+        # Filter to the required data
+        data = record['dynamodb']['NewImage']
+
+        # Pull required attributes from event object
+        id          = data['id']['S']
+        weight_data = data['weight']['L']
+        value_data  = data['value']['L']
+
+        # Convert the object array into a usuable python list
+        weight = build_array(weight_data)
+        value = build_array(value_data)
+
+        print(weight)
+        print(value)
+
+        # Run core calculations and round output
+        output = calculate(weight, value)
+
+        #
+        output = int(round(output))
+
+        print(output)
+
+        # Write result to output table
+        put_dynamo(id, str(output))
+
+    else:
+        print('Not an insert event - Skipping')
 
     body = {
         "message": "Serverless function executed successfully!",
@@ -113,8 +243,8 @@ def main(event, context):
     response = {
         "statusCode": 200,
         "body": json.dumps(body),
-        "CumulativeValue": total,
-        "User":user
+        "CumulativeValue": 'debug',
+    #    "Data": data
     }
 
     return response
